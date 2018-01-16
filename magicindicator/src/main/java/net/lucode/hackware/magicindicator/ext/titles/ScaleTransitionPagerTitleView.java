40e37ff1bed9c64945fbe94835fe1d7b35b1f687
Leeeast <lidongliang_1990@126.com>
2018-01-16 09:51:50 +0800

    tab缩放调整

--- magicindicator/src/main/java/net/lucode/hackware/magicindicator/ext/titles/ScaleTransitionPagerTitleView.java
+++ magicindicator/src/main/java/net/lucode/hackware/magicindicator/ext/titles/ScaleTransitionPagerTitleView.java
@@ -10,7 +10,7 @@ import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.ColorT
  * Created by hackware on 2016/6/26.
  */
 public class ScaleTransitionPagerTitleView extends ColorTransitionPagerTitleView {
-    private float mMinScale = 0.75f;
+    private float mMinScale = 0.96f;
 
     public ScaleTransitionPagerTitleView(Context context) {
         super(context);
